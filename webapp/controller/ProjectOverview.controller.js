2c0a2068a7c89ea406c5b181e88583147f182a41
Saplistic <167242143+Saplistic@users.noreply.github.com>
2024-04-26 14:24:18 +0000

    Mapped Gantt to ODataV4 service, and implement functionality to move start- & enddate

--- webapp/controller/ProjectOverview.controller.js
+++ webapp/controller/ProjectOverview.controller.js
@@ -1,5 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	"sap/gantt/misc/Utility",
+	"sap/gantt/simple/Relationship",
+	"sap/ui/core/Core"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -8,8 +11,17 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("ui5staffingapp.controller.ProjectOverview", {
-            onInit: function () {
+            onInit: function () {},
 
-            }
+            onShapeResize: function(oEvent) {
+                var oShape = oEvent.getParameter("shape");
+                var aNewTime = oEvent.getParameter("newTime");
+                var sBindingPath = oShape.getBindingContext("projectAssignment").getPath();
+
+                var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({pattern : "yyyy-MM-dd" });
+
+                oShape.getBindingContext("projectAssignment").setProperty(sBindingPath + "/DateStarted", dateFormat.format(aNewTime[0]));
+                oShape.getBindingContext("projectAssignment").setProperty(sBindingPath + "/DateEnded", dateFormat.format(aNewTime[1]));
+            },
         });
     });
